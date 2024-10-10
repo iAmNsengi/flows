03f2713ece73bd037813348781fbe7b07b35e72e
Eliezer Nsengi <iamnsengi@icloud.com>
2024-10-10 11:07:23 -0500

    feat: added a map

--- src/components/Flow1.jsx
+++ src/components/Flow1.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import "@xyflow/react/dist/style.css"
-import { ReactFlow } from '@xyflow/react'
+import { addEdge, Background, Controls, MiniMap, ReactFlow, useEdgesState, useNodesState } from '@xyflow/react'
+import { useCallback } from "react"
 
 const initialNodes = [
     {id:'1', position:{x:0, y:0}, data:{label:"1"}},
@@ -10,10 +10,19 @@ const initialNodes = [
 const initialEdges = [{id:'e1-2', source:'1',target:'2'}]
 
 const Flow1 = () => {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+    const onConnect = useCallback((params)=> setEdges((eds)=> addEdge(params, eds)), [setEdges])
+
   return (
     <div style={{width:'100vw', height:"100vh"}}>
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+        <ReactFlow nodes={initialNodes} edges={initialEdges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect}>
 
+            <Controls />
+            <MiniMap />
+            <Background variant="dots" gap={12} size={1}/>
+        </ReactFlow>
     </div>
   )
 }
